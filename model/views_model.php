1deecb2b21f45ba045bc450f89b6d7bdd1fc7159
cintia0719 <cintiamendoza370@gmail.com>
2025-09-14 10:56:56 -0500

    views_model.php

--- model/views_model.php
+++ model/views_model.php
@@ -1,7 +1,7 @@
 <?php
 class viewModel{
     protected static function get_view($view){
-        $white_list = ["home", "products", "users", "new-user", "edit-user","categoria"];
+        $white_list = ["home", "products", "users", "new-user", "edit-user","categoria","products"];
         if (in_array($view, $white_list)) {
             if (is_file("./view/".$view.".php")) {
                 $content = "./view/".$view.".php";
